89831cb603672e95f2c72a961464d005c4672715
James <James@LAPTOP-AHSPVLIQ>
2021-09-15 14:08:55 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace PlayGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.startText = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(185, 49);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(204, 100);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start Game";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // startText
+            // 
+            this.startText.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startText.ForeColor = System.Drawing.Color.White;
+            this.startText.Location = new System.Drawing.Point(12, 194);
+            this.startText.Name = "startText";
+            this.startText.Size = new System.Drawing.Size(579, 65);
+            this.startText.TabIndex = 1;
+            this.startText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.startText.Click += new System.EventHandler(this.startText_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 344);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(595, 440);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.startText);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label startText;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGame
+{
+    
+    public partial class Form1 : Form
+    {
+        int timer;
+        SoundPlayer countDownSound;
+        SoundPlayer startSound;
+        int loopNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Hide();
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            startButton.Hide(); //Hide the button
+            countDownSound = new SoundPlayer(Properties.Resources.countDownSound);
+            startSound = new SoundPlayer(Properties.Resources.startSound);
+
+            loopNum = timer;
+            RefreshText();
+
+        }
+
+        private void RefreshText()
+        {
+            for (int i = 0; i < loopNum; i++)
+            {
+                countDownSound.Play();
+                startText.Text = "Game Starting in: " + timer;
+                Refresh();
+                timer--;
+                Thread.Sleep(1000);
+
+                if (timer == 0)
+                {
+                    startSound.Play();
+                    startText.Text = "Go!!!";
+                    this.BackColor = Color.Chartreuse;
+                    Refresh();
+                }
+            }
+            
+        }
+
+        private void startText_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string timerString = textBox1.Text;
+            timer = Int32.Parse(timerString);
+        }
+    }
+}
